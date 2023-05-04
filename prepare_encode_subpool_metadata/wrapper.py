2b459d887a7d6b4dc4fec3d721bf5124ed2446da
Diane Trout <diane@ghic.org>
2023-05-03 17:05:36 -0700

    Force snakmake input filenames to be of type string

--- prepare_encode_subpool_metadata/wrapper.py
+++ prepare_encode_subpool_metadata/wrapper.py
@@ -2,12 +2,16 @@ import pandas
 
 from encoded_client.metadata import generate_star_solo_subpool_sheet
 
+aligned_bam = str(snakemake.input.bam)
+gene_unique_raw = str(snakemake.input.gene_unique_raw)
+gene_multi_raw = str(snakemake.input.gene_multi_raw)
+sj_unique_raw = str(snakemake.input.sj_unique_raw)
 
 records = {
-    "alignments": snakemake.input.bam,
-    "unfiltered sparse gene count matrix of unique reads": snakemake.input.gene_unique_raw,
-    "unfiltered sparse gene count matrix of all reads": snakemake.input.gene_multi_raw,
-    "unfiltered sparse splice junction count matrix of unique reads": snakemake.input.sj_unique_raw,
+    "alignments": aligned_bam,
+    "unfiltered sparse gene count matrix of unique reads": gene_unique_raw,
+    "unfiltered sparse gene count matrix of all reads": gene_multi_raw,
+    "unfiltered sparse splice junction count matrix of unique reads": sj_unique_raw,
 }
 library_id = snakemake.wildcards.library_id
 
